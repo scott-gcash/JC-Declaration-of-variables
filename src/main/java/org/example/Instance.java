37d05f0fd67c2de7525767d1851facdfda00d8f9
scott-gcash <nicholasscott.campos@gcash.com>
2025-07-31 15:55:56 +0800

    Added local, instance, and static variables

---
+++ src/main/java/org/example/Instance.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Instance {
+    private final int instanceBinary = 0b101101;
+    private final int instanceOctal = 0065;
+    private final double instanceDouble = 1234.95;
+
+    public int getInstanceBinary() {
+        return instanceBinary;
+    }
+
+    public int getInstanceOctal() {
+        return instanceOctal;
+    }
+
+    public double getInstanceDouble() {
+        return instanceDouble;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,44 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    static final int staticHex = 0x1A2B;
+    static final byte staticByte = 1;
+    static final String staticString = "Progress, not perfection.";
+
+    public static void main(String[] args) {
+        System.out.println("Local Variables: ");
+        getLocalVariables();
+
+        System.out.println("\nInstance Variables: ");
+        Instance instance = new Instance();
+        System.out.printf("instanceBinary = %d \n", instance.getInstanceBinary());
+        System.out.printf("instanceOctal = %d \n", instance.getInstanceOctal());
+        System.out.printf("instanceDouble = %.2f \n", instance.getInstanceDouble());
+
+        System.out.println("\nStatic Variables: ");
+        getStaticVariables();
+    }
+
+    public static void getLocalVariables(){
+        int localDecimal = 100;
+        int localHex = 0xFF;
+        int localBinary = 13;
+        int localOctal = 014;
+        int localLong = 10000000;
+        String localString = "Every step I take gets me closer to my goal.";
+
+        System.out.printf("localDecimal = %d \n", localDecimal);
+        System.out.printf("localHex = %d \n", localHex);
+        System.out.printf("localBinary = %d \n", localBinary);
+        System.out.printf("localOctal = %d \n", localOctal);
+        System.out.printf("localLong = %d \n", localLong);
+        System.out.printf("localString = %s \n", localString);
+    }
+
+    public static void getStaticVariables(){
+        System.out.printf("staticHex = %d \n", staticHex);
+        System.out.printf("staticByte = %d \n", staticByte);
+        System.out.printf("staticString = %s \n", staticString);
     }
 }
